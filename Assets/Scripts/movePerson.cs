cca81a0671ede37c53512a7cb6a1e0027550a729
JamesSince97 <james.robson.junior@gmail.com>
2018-09-17 22:33:02 -0400

    Movimentação de Personagem

--- Assets/Scripts/movePerson.cs
+++ Assets/Scripts/movePerson.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class movePerson : MonoBehaviour {
 
+    CharacterController objetoCharControler;
+	float giro = 300.0f;
+	Vector3 vetorDirecao = new Vector3(0,0,0);
+	float velocidade = 5.0f;
+	private float pulo = 5.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+		objetoCharControler = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * velocidade;
+		transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * giro * Time.deltaTime,0));
+		objetoCharControler.Move(forward * Time.deltaTime);
+		objetoCharControler.SimpleMove(Physics.gravity);
+		if (Input.GetButton("Jump")){
+			if(objetoCharControler.isGrounded == true) {
+				vetorDirecao.y = pulo;			
+				}
+		}
+		vetorDirecao.y -= 3.0f * Time.deltaTime;
+		objetoCharControler.Move(vetorDirecao * Time.deltaTime);
+
 	}
 }
