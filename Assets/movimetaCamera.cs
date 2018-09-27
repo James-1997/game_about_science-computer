a8667f672e9bf15f0b208d7e29334b17a6ca70f3
JamesSince97 <james.robson.junior@gmail.com>
2018-09-27 16:20:50 -0400

    Camera de Jogo no Personagem

---
+++ Assets/movimetaCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimetaCamera : MonoBehaviour {
+
+	Vector3 offSet;
+	public GameObject jogador;
+	
+	void Start ()
+	{
+		offSet = transform.position - jogador.transform.position;
+	}
+	
+	void Update ()
+	{
+		transform.position = jogador.transform.position + offSet;
+	}
+}
