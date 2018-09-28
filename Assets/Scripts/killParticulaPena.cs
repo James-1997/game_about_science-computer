2737463da06a3cf054eea9886da924b328c7cb67
JamesSince97 <james.robson.junior@gmail.com>
2018-09-27 22:20:28 -0400

    Destroy Particules Clone

---
+++ Assets/Scripts/killParticulaPena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killParticulaPena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		InvokeRepeating("killParticula", 1, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void killParticula () {
+		Destroy(this.gameObject);
+	}
+}
